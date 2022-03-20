e7089b9c6ff07e4af0bff0138cd05e320b7f7cc8
webdevterri <32005382+webdevterri@users.noreply.github.com>
2022-03-20 01:31:14 -0500

    Wrote code for the conversion rate formulas in JS

---
+++ index.js
@@ -0,0 +1,20 @@
+let num = 20;
+
+// Unit Conversion Formulas
+// Length (Meters/Feet)
+let meter = num;
+let feet = num;
+const lengthInMeters = feet * 0.30
+const lengthInFeet = meter * 3.28
+
+// Volume (Liters/Gallons)
+let liters = num;
+let gallons = num;
+const volumeInLiters = gallons * 3.785
+const volumeInGallons = liters * 0.26
+
+// Mass (Kilograms/Pounds)
+let kilograms = num;
+let pounds = num;
+const massInKilograms = pounds * 0.45
+const massInPounds = kilograms * 2.20
