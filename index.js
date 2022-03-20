28d3288d51dc06da27d1fa67a7f9b8ec5f5808e5
webdevterri <32005382+webdevterri@users.noreply.github.com>
2022-03-20 01:58:19 -0500

    Wrote most JS and CSS

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 let num = 20;
 
+document.getElementById("inputNum").innerHTML = `
+<p class="inputNumDisplay">${num}</p>
+`;
+
 // Unit Conversion Formulas
 // Length (Meters/Feet)
 let meter = num;
 let feet = num;
-const lengthInMeters = feet * 0.30
-const lengthInFeet = meter * 3.28
+const lengthInMeters = feet * 0.3;
+const lengthInFeet = meter * 3.28;
 
 // Volume (Liters/Gallons)
 let liters = num;
 let gallons = num;
-const volumeInLiters = gallons * 3.785
-const volumeInGallons = liters * 0.26
+const volumeInLiters = gallons * 3.785;
+const volumeInGallons = liters * 0.26;
 
 // Mass (Kilograms/Pounds)
 let kilograms = num;
 let pounds = num;
-const massInKilograms = pounds * 0.45
-const massInPounds = kilograms * 2.20
+const massInKilograms = pounds * 0.45;
+const massInPounds = kilograms * 2.2;
+
+document.getElementById("conversionResults").innerHTML = `
+<h2>Length (Meter/Feet)</h2>
+<p>${lengthInMeters} meters = ${lengthInFeet} feet | ${lengthInFeet} feet = ${lengthInMeters} meters</p>
+<h2>Volume (Liters/Gallons)</h2>
+<p>${volumeInLiters} liters = ${volumeInGallons} gallons | ${volumeInGallons} gallons = ${volumeInLiters} liters</p>
+<h2>Mass (Kilograms/Pounds)</h2>
+<p>${massInKilograms} kilograms = ${massInPounds} pounds | ${massInPounds} pounds = ${massInKilograms} kilograms</p>
+`;
+
+// Round to 3 decimal points like: 37.245
